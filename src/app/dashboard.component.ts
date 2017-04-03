24af3aaebc0d71ed0ef682916e2c8a7c22350486
Suarez <andsuarez@usaid.gov>
2017-04-03 15:18:30 -0400

    partially added route params

--- src/app/dashboard.component.ts
+++ src/app/dashboard.component.ts
@@ -1,22 +1,25 @@
 import { Component } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, Router } from '@angular/router';
 import { Articles } from './articles';
 import { ARTICLES } from './mock-articles';
+import { ArticleService } from './article.service';
 
 @Component({
     selector: 'dashboard',
     templateUrl: 'dashboard.component.html',
-    styleUrls: ['dashboard.component.css']
+    styleUrls: ['dashboard.component.css'],
+    providers: [ArticleService]
 })
 
 export class DashboardComponent{
     articles = ARTICLES;
 
-    // constructor(
-    //     private router: Router;
-    // ) {}
+    constructor(
+        private router: Router,
+        private service: ArticleService
+    ) {}
 
-    // selectArticle(article: Articles) {
-    //     this.router.navigate(['/article', articles.id]);
-    // }
+    selectArticle(article: Articles) {
+        this.router.navigate(['/article', article.id]);
+    }
 }
